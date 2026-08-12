987d19311364757b127e5716b7078496e74cb3e3
Nashrullah Fathul Qoriib <nashrullah.122140162@student.itera.ac.id>
2026-08-12 09:17:57 +0700

    init

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@astryxdesign/core/reset.css";
+import "@astryxdesign/core/astryx.css";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
